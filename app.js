caf20bd3051c30b1853c72b4fe07879953c1601f
Ahmetozcan28 <mrkronoss@hotmail.com>
2023-11-21 15:31:09 +0300

    update: footer slider

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const paginationsNav = document.querySelector(".js-slider-paginations");
 const paginationsİtems = document.querySelectorAll(".paginations-item");
 const footerImage = document.querySelector(".footer-image");
-const footerButton = document.querySelector(".footer-button");
-const photo = [
-  "url(https://eu1.bumbcdn.com/ssc/static-eu.bumble.com/s4/files/e/2.1.mgfBZES6-elQXZ2rKcYFyYFjSaTHbrQohp3QGrB8K6u0rOZtRlih8huDlDxUK76-6yf8jArxeppypVlb6mt4zw/)center/cover",
-  "url(https://eu1.bumbcdn.com/ssc/static-eu.bumble.com/s3/files/e/2.1.mgfBZES6-elQXZ2rKcYFyYHl5uKF8Q4dUIo8VbO283P1szl7-Y8m41oSqoxOg6yBzMTC9ht8tDFXpR5kmHv4ZQ/)center/cover",
-  "url(https://eu1.bumbcdn.com/ssc/static-eu.bumble.com/s2/files/e/2.1.mgfBZES6-elQXZ2rKcYFyVIUzkBgNY1l_ALOzgOYIrErTLCyYW6pcKT_937iJ6NP__nXdUZUR6K8GER3cer-AQ/)center/cover",
-];
+const footerImageTransition = document.querySelector(".transitons-div-footer");
 
+/*
 let i = 0;
 const time = 3000;
 const images = [];
@@ -31,18 +27,8 @@ const images = [];
             }
           });
       });*/
+/*
 
-function changeFooterImage(index) {
-  footerImage.style.background = images[i];
-
-  for (let j = 0; j < paginationsİtems.length; j++) {
-    if (j === index) {
-      paginationsİtems[j].classList.add("is-active");
-    } else {
-      paginationsİtems[j].classList.remove("is-active");
-    }
-  }
-}
 
 function startImageSlider() {
   setInterval(() => {
@@ -55,3 +41,20 @@ function startImageSlider() {
     }
   }, time);
 }
+
+startImageSlider()*/
+
+paginationsNav.addEventListener("click", (event) => {
+  if (event.target !== paginationsNav)
+    paginationsİtems.forEach((item, index) => {
+      if (event.target === item) {
+        const pageIndex = index;
+        const pageWidth = footerImage.offsetWidth;
+
+        footerImageTransition.scrollLeft = pageIndex * pageWidth ;
+        item.classList.add("is-active");
+      } else {
+        item.classList.remove("is-active");
+      }
+    });
+});
