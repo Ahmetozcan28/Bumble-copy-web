28e33b1388c469f267ddf3fb13c3fe2f09f0269e
Ahmetozcan28 <mrkronoss@hotmail.com>
2023-11-21 12:06:51 +0300

    slider card footer img

--- app.js
+++ app.js
@@ -7,14 +7,51 @@ const photo = [
   "url(https://eu1.bumbcdn.com/ssc/static-eu.bumble.com/s3/files/e/2.1.mgfBZES6-elQXZ2rKcYFyYHl5uKF8Q4dUIo8VbO283P1szl7-Y8m41oSqoxOg6yBzMTC9ht8tDFXpR5kmHv4ZQ/)center/cover",
   "url(https://eu1.bumbcdn.com/ssc/static-eu.bumble.com/s2/files/e/2.1.mgfBZES6-elQXZ2rKcYFyVIUzkBgNY1l_ALOzgOYIrErTLCyYW6pcKT_937iJ6NP__nXdUZUR6K8GER3cer-AQ/)center/cover",
 ];
-paginationsNav.addEventListener("click", (event) => {
-  if (event.target !== paginationsNav)
-    paginationsİtems.forEach((item, index) => {
-      if (event.target === item) {
-        item.classList.add("is-active");
-        footerImage.style.background = photo[index];
-      } else {
-        item.classList.remove("is-active");
-      }
-    });
-});
+
+let i = 0;
+const time = 3000;
+const images = [];
+
+(images[0] =
+  "url(https://eu1.bumbcdn.com/ssc/static-eu.bumble.com/s4/files/e/2.1.mgfBZES6-elQXZ2rKcYFyYFjSaTHbrQohp3QGrB8K6u0rOZtRlih8huDlDxUK76-6yf8jArxeppypVlb6mt4zw/)center/cover"),
+  (images[1] =
+    "url(https://eu1.bumbcdn.com/ssc/static-eu.bumble.com/s3/files/e/2.1.mgfBZES6-elQXZ2rKcYFyYHl5uKF8Q4dUIo8VbO283P1szl7-Y8m41oSqoxOg6yBzMTC9ht8tDFXpR5kmHv4ZQ/)center/cover"),
+  (images[2] =
+    "url(https://eu1.bumbcdn.com/ssc/static-eu.bumble.com/s2/files/e/2.1.mgfBZES6-elQXZ2rKcYFyVIUzkBgNY1l_ALOzgOYIrErTLCyYW6pcKT_937iJ6NP__nXdUZUR6K8GER3cer-AQ/)center/cover");
+
+/*
+    paginationsNav.addEventListener("click", (event) => {
+        if (event.target !== paginationsNav)
+          paginationsİtems.forEach((item, index) => {
+            if (event.target === item) {
+              item.classList.add("is-active");
+              footerImage.style.background = photo[index];
+            } else {
+              item.classList.remove("is-active");
+            }
+          });
+      });*/
+
+function changeFooterImage(index) {
+  footerImage.style.background = images[i];
+
+  for (let j = 0; j < paginationsİtems.length; j++) {
+    if (j === index) {
+      paginationsİtems[j].classList.add("is-active");
+    } else {
+      paginationsİtems[j].classList.remove("is-active");
+    }
+  }
+}
+
+function startImageSlider() {
+  setInterval(() => {
+    changeFooterImage(i);
+
+    if (i < images.length - 1) {
+      i++;
+    } else {
+      i = 0;
+    }
+  }, time);
+}
